743b83cffc1092d278a8042fa25d4079802cb2af
Gilberto Albino <contato@gilbertoalbino.com>
2014-07-31 11:31:42 -0300

    Update jquery.querystring.js

--- jquery.querystring.js
+++ jquery.querystring.js
@@ -1,16 +1,11 @@
-jQuery.fn.queryString = function(params, options) 
-{
-    var defaults = {
-        start : ''
-    };
-    
-    var settings = jQuery.extend({}, defaults, options);
-    
-    if(params !== undefined) {
-        var queryString = new Array();
-        for(var name in params) {
-            queryString.push([name, params[name]].join('='));
+(function($){
+    $.queryString = function(params, start) {
+        if(params !== undefined) {
+            var queryString = new Array();
+            for(var name in params) {
+                queryString.push([name, params[name]].join('='));
+            }
+            return ((start === undefined)? '' : start) + queryString.join('&');
         }
-        return settings.start + queryString.join('&'); 
-    }
-};
+    };
+})(jQuery);
